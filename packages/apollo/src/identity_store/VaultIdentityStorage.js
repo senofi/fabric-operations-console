1ab958ad4e1edc8555b4936ad8d869c5df989023
Lyubomir Bandrov <lyubomirbandrov@yahoo.com>
2023-02-26 21:53:45 +0200

    IDL-60 Remove not needed console logs and fix error rethrow
    Signed-off-by: Lyubomir Bandrov <lyubomir.bandrov@senofi.ca>

--- packages/apollo/src/identity_store/VaultIdentityStorage.js
+++ packages/apollo/src/identity_store/VaultIdentityStorage.js
@@ -2,44 +2,20 @@ import { RestApi } from '../rest/RestApi';
 
 class VaultIdentityStorage {
 	async save(key, data) {
-		try {
-			let url = '/api/v3/vault/identity';
+		let url = '/api/v3/vault/identity';
 
-			return await RestApi.put(url, data);
-		} catch (error) {
-			if (error.statusCode === 503) {
-				return RestApi.get('/api/v3/components?deployment_attrs=included');
-			} else {
-				throw error;
-			}
-		}
+		return await RestApi.put(url, data);
 	}
 
 	async get() {
-		try {
-			let url = '/api/v3/vault/identity';
-			const results = await RestApi.get(url);
-			return results;
-		} catch (error) {
-			if (error.statusCode === 503) {
-				return RestApi.get('/api/v3/components?deployment_attrs=included');
-			} else {
-				throw error;
-			}
-		}
+		let url = '/api/v3/vault/identity';
+		const results = await RestApi.get(url);
+		return results;
 	}
 
 	async removeIdentity(name) {
-		try {
-			let url = `/api/v3/vault/identity/${name}`;
-			await RestApi.delete(url);
-		} catch (error) {
-			if (error.statusCode === 503) {
-				return RestApi.get('/api/v3/components?deployment_attrs=included');
-			} else {
-				throw error;
-			}
-		}
+		let url = `/api/v3/vault/identity/${name}`;
+		await RestApi.delete(url);
 	}
 }
 
